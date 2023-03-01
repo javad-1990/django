9d23f441069a219c601e11307d0661a83d37f94d
javad-1990 <j.borhani1369@gmail.com>
2023-03-01 12:17:59 +0330

    nine

--- account/views.py
+++ account/views.py
@@ -3,8 +3,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from pages.models import Article
+
 # Create your views here.
 class ArticleList(LoginRequiredMixin, ListView):
-	queryset = Article.objects.all()
 	template_name = "registration/home.html"
 
+	def get_queryset(self):
+		if self.request.user.is_superuser:
+			return Article.objects.all()
+		else:
+			return Article.objects.filter(author=self.request.user)
